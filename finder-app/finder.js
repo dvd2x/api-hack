451c49b5fce1405fb393b48156b006a186f9d870
David Piper <black.god1930@gmail.com>
2019-06-28 17:20:57 -0400

    updated api keyword

--- finder-app/finder.js
+++ finder-app/finder.js
@@ -1,46 +1,47 @@
-'use strict';
+"use strict";
 
 // put your own value below!
-const apiKey = 'AIzaSyCGKrLxvpot6hrekFHQTPaCGeOFj92T3ao'; 
-const searchURL = 'https://www.googleapis.com/youtube/v3/search';
-
+const apiKey = "AIzaSyBF1mbOSX_n4nPp1vOy2F6rnQcjtjJKjfw";
+const searchURL = "https://www.googleapis.com/youtube/v3/search";
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  const queryItems = Object.keys(params).map(
+    key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
 }
 
 function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
-  $('#results-list').empty();
+  $("#results-list").empty();
   // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
+  for (let i = 0; i < responseJson.items.length; i++) {
+    // for each video object in the items
+    //array, add a list item to the results
     //list with the video title, description,
     //and thumbnail
-    $('#results-list').append(
+    $("#results-list").append(
       `<li><h3>${responseJson.items[i].snippet.title}</h3>
       <p>${responseJson.items[i].snippet.description}</p>
       <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
       </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-};
+    );
+  }
+  //display the results section
+  $("#results").removeClass("hidden");
+}
 
-function getYouTubeVideos(query, maxResults=10) {
+function getYouTubeVideos(query, maxResults = 10) {
   const params = {
     key: apiKey,
     q: query,
-    part: 'snippet',
+    part: "snippet",
     maxResults,
-    type: 'video'
+    type: "video"
   };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+  const queryString = formatQueryParams(params);
+  const url = searchURL + "?" + queryString;
 
   console.log(url);
 
@@ -53,15 +54,15 @@ function getYouTubeVideos(query, maxResults=10) {
     })
     .then(responseJson => displayResults(responseJson))
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
     });
 }
 
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit(event => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
+    const searchTerm = $("#js-search-term").val();
+    const maxResults = $("#js-max-results").val();
     getYouTubeVideos(searchTerm, maxResults);
   });
 }
